a62b6b172a1ef32927cca383daf0be8264160c97
DevGSCTech <appdev@globalstaffcapital.com>
2024-06-25 15:52:01 -0400

    commit before adding authenticator to tabs_layout.tsx

--- workrightdev/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ workrightdev/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "workrightdevfb9e4210": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
